1ad3d6c2f4058f9209ee91a90f835996f058bb0d
Skanda19cs137 <71484498+Skanda19cs137@users.noreply.github.com>
2021-10-08 15:28:19 +0530

    Blink and traffic light programs

---
+++ 08-10-21/traffic/traffic.ino
@@ -0,0 +1,25 @@
+int r=13;
+int y=12;
+int g=11;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(r, OUTPUT);
+  pinMode(y, OUTPUT);
+  pinMode(g, OUTPUT);
+  digitalWrite(r, HIGH); 
+  digitalWrite(y, LOW); 
+  digitalWrite(g, LOW); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(r, HIGH);
+  delay(3000);
+  digitalWrite(r, LOW); 
+  digitalWrite(y, HIGH);   
+  delay(1000);
+  digitalWrite(y, LOW); 
+  digitalWrite(g, HIGH);   
+  delay(5000);
+  digitalWrite(g, LOW);  
+}
